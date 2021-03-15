51fd783a2997591ec6a24c070a616c437adcad37
dougstorm <dougstorm@users.noreply.github.com>
2021-03-15 16:32:57 -0600

    feat: add extra fetch methods

---
+++ src/utils/strategies.ts
@@ -0,0 +1,64 @@
+import {
+    Multicall,
+    ContractCallResults,
+    ContractCallContext,
+    ContractCallReturnContext,
+  } from 'ethereum-multicall';
+import { utils } from 'ethers';
+
+import { getEthersDefaultProvider } from './ethers';
+import { Strategy } from '../types';
+import { mapContractCalls } from './vaults';
+
+import StratABI from './ABI/Strategy.json';
+
+const STRAT_VIEW_METHODS = [
+    'apiVersion',
+    'emergencyExit',
+    'isActive',
+    'keeper',
+    'rewards',
+    'strategist',
+    'name',
+    'vault'
+];
+
+export const getStrategies = async (addresses: string[]): Promise<Strategy[]> => {
+    if (addresses.length === 0) {
+        throw new Error('Error: expect a valid strategy address');
+    }
+
+    addresses.forEach(address => {
+        if (!address || !utils.isAddress(address)) {
+            throw new Error('Error: expect a valid strategy address');
+        }
+    });
+
+    const provider = getEthersDefaultProvider();
+
+    const multicall = new Multicall({ ethersProvider: provider });
+
+    const stratCalls: ContractCallContext[] = addresses.map((address) => {
+        const calls = STRAT_VIEW_METHODS.map(method => ({ reference: method, methodName: method, methodParameters: [] }));
+        return {
+                reference: address,
+                contractAddress: address,
+                abi: StratABI.abi,
+                calls,
+        }
+        
+    });
+    
+    const results: ContractCallResults = await multicall.call(stratCalls);
+    const mappedStrategies: Strategy[]  = addresses.map((address) => {
+        const stratData = results.results[address];
+        let mappedStrat: any = mapContractCalls(stratData);
+
+        return {
+            ...mappedStrat,
+            address,
+        };
+    });  
+
+    return mappedStrategies;
+}
