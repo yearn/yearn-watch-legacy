50914c97b48fad8242d118d3193dfb431338f23e
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2021-06-08 14:47:09 -0400

    Add warnings to main vaults page.

---
+++ src/components/common/HtmlTooltip/index.ts
@@ -0,0 +1,12 @@
+import { withStyles } from '@material-ui/core/styles';
+import { Tooltip } from '@material-ui/core';
+
+export const HtmlTooltip = withStyles((theme) => ({
+    tooltip: {
+        backgroundColor: '#f5f5f9',
+        color: 'rgba(0, 0, 0, 0.87)',
+        maxWidth: 220,
+        fontSize: theme.typography.pxToRem(12),
+        border: '1px solid #dadde9',
+    },
+}))(Tooltip);
