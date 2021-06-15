3b0d91bee8f427ba89cc87bd24b7c74c9cea7c1e
Storm0x <6074987+dougstorm@users.noreply.github.com>
2021-06-15 15:07:12 -0600

    fix: display amount decimals (#70)
    Co-authored-by: dougstorm <dougstorm@users.noreply.github.com>

--- src/utils/__tests__/commonUtils.test.ts
+++ src/utils/__tests__/commonUtils.test.ts
@@ -2,13 +2,16 @@ import { displayAmount } from '../commonUtils';
 
 describe('#DisplayAmount', () => {
     it.each([
-        ['61776792004700431', '0.06178'],
-        ['559434119616550549964644', '559,434'],
-        ['0', '0'],
-    ])('should display %i to be %s', (amt, expected) => {
-        const decimals = 18;
-
-        const res = displayAmount(amt, decimals);
-        expect(res).toBe(expected);
-    });
+        ['61776792004700431', 18, '0.06178'],
+        ['559434119616550549964644', 18, '559,434.11962'],
+        ['0', 18, '0'],
+        ['553183935', 8, '5.53184'],
+        ['20819427', 8, '0.20819'],
+    ])(
+        'should display %i with %s decimals to be %j',
+        (amt, decimals, expected) => {
+            const res = displayAmount(amt, decimals);
+            expect(res).toBe(expected);
+        }
+    );
 });
