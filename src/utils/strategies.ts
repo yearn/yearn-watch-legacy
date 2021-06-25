c4ebdafdc495e59707feb35f59d235ffb64ec8c7
storming0x <storming0x@users.noreply.github.com>
2021-06-24 18:23:12 -0600

    fix: add todo comment

--- src/utils/strategies.ts
+++ src/utils/strategies.ts
@@ -63,6 +63,8 @@ const TOKEN_VIEW_METHODS: string[] = ['decimals', 'symbol', 'name'];
 const filterList = ['0x2923a58c1831205c854dbea001809b194fdb3fa5'];
 const buildViewMethodsCall = (strategies: string[]): ContractCallContext[] => {
     return strategies.map((stratAddres) => {
+        // TODO: this is a hack since a strategy is failing on this specific call
+        // remove after on chain fix
         const filterCalls = STRAT_VIEW_METHODS.filter((viewMethod) => {
             return !(
                 filterList.includes(stratAddres.toLowerCase()) &&
