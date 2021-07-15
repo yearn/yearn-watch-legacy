b9fadfe5ac66130504f268f54ccc3b51a06ff612
storming0x <storming0x@users.noreply.github.com>
2021-07-15 10:00:00 -0600

    feat: refaactor human readable date texts

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -2,6 +2,12 @@ import dayjs from 'dayjs';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const moment = require('moment');
+import * as moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
+
+momentDurationFormatSetup(moment);
 dayjs.extend(LocalizedFormat);
 dayjs.extend(relativeTime);
 
@@ -25,6 +31,16 @@ export const unixMsToIsoString = (timestamp: string): string => {
 };
 
 export const toHumanDateText = (timestamp: string): string => {
+    if (!isValidTimestamp(timestamp)) return '';
+    const unixTimestamp = moment.unix(parseInt(timestamp));
+    const now = moment.default();
+
+    return moment
+        .duration(now.diff(unixTimestamp))
+        .format('d [days] hh [hours] m [minutes]', { trim: false });
+};
+
+export const toHumanDateTextOld = (timestamp: string): string => {
     if (!isValidTimestamp(timestamp)) return '';
     const unixTimestamp = dayjs.unix(parseInt(timestamp));
     return unixTimestamp.toNow(true);
