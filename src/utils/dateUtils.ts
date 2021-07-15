afda109722f2f757efa18cd5b5750e65c3f880cf
Storming0x <6074987+storming0x@users.noreply.github.com>
2021-07-15 11:52:45 -0600

    Feat/improve dates (#92)
    * feat: refaactor human readable date texts
    
    * fix: refactor date utils to use moment and remove dayjs
    
    Co-authored-by: storming0x <storming0x@users.noreply.github.com>

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,17 +1,16 @@
-import dayjs from 'dayjs';
-import LocalizedFormat from 'dayjs/plugin/localizedFormat';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import { BigNumber } from 'ethers';
+import * as moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
 
-dayjs.extend(LocalizedFormat);
-dayjs.extend(relativeTime);
+momentDurationFormatSetup(moment);
 
 export const isValidTimestamp = (timestamp: string): boolean => {
-    return dayjs.unix(parseInt(timestamp)).isValid();
+    return moment.unix(parseInt(timestamp)).isValid();
 };
 // Unix Timestamp (seconds)
 export const toIsoString = (timestamp: string): string => {
     if (!isValidTimestamp(timestamp)) return '';
-    const unixTimestamp = dayjs.unix(parseInt(timestamp));
+    const unixTimestamp = moment.unix(parseInt(timestamp)).utc();
 
     return unixTimestamp.toISOString();
 };
@@ -19,13 +18,30 @@ export const toIsoString = (timestamp: string): string => {
 // Unix Timestamp (milliseconds)
 export const unixMsToIsoString = (timestamp: string): string => {
     if (!isValidTimestamp(timestamp)) return '';
-    const unixTimestamp = dayjs(parseInt(timestamp));
+    const unixTimestamp = moment.default(parseInt(timestamp)).utc();
 
     return unixTimestamp.toISOString();
 };
 
 export const toHumanDateText = (timestamp: string): string => {
     if (!isValidTimestamp(timestamp)) return '';
-    const unixTimestamp = dayjs.unix(parseInt(timestamp));
-    return unixTimestamp.toNow(true);
+    const unixTimestamp = moment.unix(parseInt(timestamp)).utc();
+    const now = moment.utc();
+
+    return moment
+        .duration(now.diff(unixTimestamp))
+        .format('d [days] hh [hours] m [minutes]', { trim: false });
+};
+
+interface LastReportLike {
+    lastReportText: string;
+    lastReport: string | BigNumber;
+}
+
+export const displayLastReport = (value?: LastReportLike): string => {
+    return value && value.lastReport
+        ? `${value.lastReportText} (${unixMsToIsoString(
+              value.lastReport.toString()
+          )})`
+        : '';
 };
