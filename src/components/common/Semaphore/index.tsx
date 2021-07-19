9effa3e6e8730a1441a49eee06fd8613a7f796a5
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2021-07-19 11:49:11 -0600

    Add risk page, and improvements.

--- src/components/common/Semaphore/index.tsx
+++ src/components/common/Semaphore/index.tsx
@@ -1,34 +1,42 @@
+import { Tooltip } from '@material-ui/core';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import React from 'react';
 
 type SemaphoreProps = {
-    value: string | number | boolean;
+    impact: number;
+    likelihood: number;
+};
+export const colors = [
+    ['#f5f514', '#f5f514', '#ff6262', '#ff6262', '#ff6262'],
+    ['#19c519', '#f5f514', '#f5f514', '#ff6262', '#ff6262'],
+    ['#19c519', '#19c519', '#f5f514', '#f5f514', '#ff6262'],
+    ['#19c519', '#19c519', '#19c519', '#f5f514', '#f5f514'],
+    ['#19c519', '#19c519', '#19c519', '#19c519', '#f5f514'],
+];
+
+export const getSemaphoreInfo = (props: SemaphoreProps) => {
+    const value = props.impact * props.likelihood;
+    const impactIndex = colors.length - props.impact;
+    const likelihoodIndex = props.likelihood - 1;
+    const color = colors[impactIndex][likelihoodIndex];
+    return {
+        value,
+        impactIndex,
+        likelihoodIndex,
+        color,
+    };
 };
 
 export const Semaphore = (props: SemaphoreProps) => {
-    const { value } = props;
-    if (typeof value === 'number') {
-        if (value > 4) {
-            return <FiberManualRecordIcon htmlColor="green" />;
-        }
-        if (value > 3) {
-            return <FiberManualRecordIcon htmlColor="yellow" />;
-        }
-        return <FiberManualRecordIcon htmlColor="red" />;
-    }
-    if (typeof value === 'boolean') {
-        if (value) {
-            return <FiberManualRecordIcon htmlColor="green" />;
-        } else {
-            return <FiberManualRecordIcon htmlColor="red" />;
-        }
-    }
-    if (typeof value === 'string') {
-        if (value !== undefined && value !== null) {
-            return <FiberManualRecordIcon htmlColor="green" />;
-        } else {
-            return <FiberManualRecordIcon htmlColor="red" />;
-        }
-    }
-    return <React.Fragment></React.Fragment>;
+    const value = props.impact * props.likelihood;
+    const impactIndex = colors.length - props.impact;
+    const likelihoodIndex = props.likelihood - 1;
+    const color = colors[impactIndex][likelihoodIndex];
+    return (
+        <Tooltip
+            title={`Impact (${props.impact}) * Likelihood (${props.likelihood}) = ${value}`}
+        >
+            <FiberManualRecordIcon htmlColor={color} />
+        </Tooltip>
+    );
 };
