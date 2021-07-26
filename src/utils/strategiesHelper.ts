5504c50b1a38c3f4c487d850a3ce20d3f3994260
Guillermo Salazar <guillesalazar@gmail.com>
2021-07-26 11:59:44 -0600

    Improve Risk Flexibility (#97)
    * Add filters to improve flexibility in grouping.
    
    * Fix issue in Query page.

--- src/utils/strategiesHelper.ts
+++ src/utils/strategiesHelper.ts
@@ -5,24 +5,47 @@ import BigNumber from 'bignumber.js';
 import { ProtocolTVL } from '../types/protocol-tvl';
 import { StrategyTVL } from '../types/strategy-tvl';
 import { getStrategies } from './strategies';
+import { flattenArrays } from './commonUtils';
+
+export const getAssetsStrategiesAddressesByFilterNames = async (
+    ...names: string[]
+): Promise<string[]> => {
+    const helper = getStrategiesHelperInstance();
+
+    const callPromises = names.map((name) =>
+        helper['assetsStrategiesAddressesByFilter(string[][])']([
+            ['KEY', 'name', 'STRING'],
+            ['VALUE', name.toLowerCase(), 'STRING'],
+            ['OPERATOR', 'LIKE'],
+        ])
+    );
+    const results = await Promise.all(callPromises);
+    const flattenResults = flattenArrays(results);
+    return Array.from(new Set(flattenResults));
+};
 
 export const getStrategyTVLsPerProtocol = async (
-    protocol: string
+    protocolName: string,
+    aliases: string[],
+    includeStrategies: string[] = [],
+    excludeStrategies: string[] = []
 ): Promise<ProtocolTVL> => {
-    const helper = getStrategiesHelperInstance();
-    const result = await helper[
-        'assetsStrategiesAddressesByFilter(string[][])'
-    ]([
-        ['KEY', 'name', 'STRING'],
-        ['VALUE', protocol.toLowerCase(), 'STRING'],
-        ['OPERATOR', 'LIKE'],
-    ]);
-    if (result.length === 0) {
-        return new ProtocolTVL(protocol, new BigNumber(0), []);
-    }
-    const strategyAddresses = result.filter(
-        (address: any) => address !== undefined
+    const isExcluded = (strategy: string) =>
+        excludeStrategies
+            .map((address) => address.toLowerCase())
+            .includes(strategy.toLowerCase());
+
+    const result = await getAssetsStrategiesAddressesByFilterNames(...aliases);
+    const filteredStrategyAddresses = result.filter(
+        (address: any) => address !== undefined && !isExcluded(address)
     );
+    const strategyAddresses = [
+        ...filteredStrategyAddresses,
+        ...includeStrategies.map((strategy) => strategy.toLowerCase()),
+    ];
+    if (strategyAddresses.length === 0) {
+        return new ProtocolTVL(protocolName, new BigNumber(0), []);
+    }
     const strategiesInfo = await getStrategies(strategyAddresses);
     const strategiesPromises = strategiesInfo.map(
         async (strategy: Strategy): Promise<StrategyTVL> => {
@@ -47,5 +70,5 @@ export const getStrategyTVLsPerProtocol = async (
     for (const strategy of strategies) {
         protocolTVL = protocolTVL.plus(strategy.estimatedTotalAssetsUsdc);
     }
-    return new ProtocolTVL(protocol, protocolTVL, strategies);
+    return new ProtocolTVL(protocolName, protocolTVL, strategies);
 };
