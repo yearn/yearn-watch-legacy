f00bc24ef039a692dbcbbb43d20b30eaa795933c
Benjamin S <bsamuels453@gmail.com>
2022-03-13 14:15:51 -0700

    add pluralizations for sync status

--- src/components/app/NavBar/SyncStatus.tsx
+++ src/components/app/NavBar/SyncStatus.tsx
@@ -3,6 +3,8 @@ import { Network } from '../../../types';
 import { Box } from '@mui/material';
 import Alert from '@mui/material/Alert';
 import Typography from '@mui/material/Typography';
+import { BigNumber } from 'ethers';
+import React from 'react';
 
 type SyncStatusProps = {
     network: Network;
@@ -19,6 +21,35 @@ export const SyncStatus = (props: SyncStatusProps) => {
         rpcData,
         rpcRequestError,
     } = useSyncStatus(network);
+
+    interface syncStatusMessageProps {
+        rpcHeight: BigNumber;
+        subgraphHeight: BigNumber;
+    }
+
+    function syncStatusMessage(props: syncStatusMessageProps): JSX.Element {
+        const subgraphLag = props.rpcHeight.sub(props.subgraphHeight);
+
+        if (subgraphLag.eq(0)) {
+            return (
+                <Typography variant="caption" align="right">
+                    Subgraph is synced with RPC
+                </Typography>
+            );
+        } else if (subgraphLag.eq(1)) {
+            return (
+                <Typography variant="caption" align="right">
+                    Subgraph is 1 block behind
+                </Typography>
+            );
+        } else {
+            return (
+                <Typography variant="caption" align="right">
+                    Subgraph is {subgraphLag.toString()} blocks behind
+                </Typography>
+            );
+        }
+    }
 
     const renderData = () => {
         if (subgraphLoading || rpcLoading) {
@@ -40,11 +71,10 @@ export const SyncStatus = (props: SyncStatusProps) => {
                     subgraphData &&
                     rpcData && (
                         <Typography variant="caption" align="right">
-                            Subgraph is{' '}
-                            {rpcData.syncHeight
-                                .sub(subgraphData.syncHeight)
-                                .toString()}{' '}
-                            blocks behind
+                            {syncStatusMessage({
+                                rpcHeight: rpcData.syncHeight,
+                                subgraphHeight: subgraphData.syncHeight,
+                            })}
                         </Typography>
                     )}
             </Box>
