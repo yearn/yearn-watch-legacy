13a90115745a29312f379b118de162d07b0a5f76
borrito <yh01dlx@gmail.com>
2022-06-06 22:59:12 +0200

    fix: change to GroupingsList

--- src/utils/risk-framework.ts
+++ src/utils/risk-framework.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { Network, NetworkId } from '../types';
-import { Groups, GroupingsList } from '../types/grouping';
+import { GroupingsList } from '../types/grouping';
 
 const RISK_GH_URL =
     'https://raw.githubusercontent.com/yearn/yearn-data-analytics/master/src/risk_framework/risks.json';
@@ -16,18 +16,14 @@ export const initRiskFrameworkScores = async (
     // try to fetch directly from github first
     const responseGithub = await axios.get(RISK_GH_URL);
     if (responseGithub.status === 200) {
-        const riskFrameworkJson = responseGithub.data as Groups;
-        return riskFrameworkJson.groups.filter(
-            (group) => group.network === networkId
-        );
+        const riskGroups = responseGithub.data as GroupingsList;
+        return riskGroups.filter((group) => group.network === networkId);
     }
     // try to fetch from the api
     const responseApi = await axios.get(RISK_API_URL);
     if (responseApi.status === 200) {
-        const riskFrameworkJson = responseApi.data as Groups;
-        return riskFrameworkJson.groups.filter(
-            (group) => group.network === networkId
-        );
+        const riskGroups = responseApi.data as GroupingsList;
+        return riskGroups.filter((group) => group.network === networkId);
     }
     // return empty array if not found
     return [];
