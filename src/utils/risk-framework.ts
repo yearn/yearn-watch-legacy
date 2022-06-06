02678d33820315fde888031aa56c9f0029b5a923
borrito <yh01dlx@gmail.com>
2022-06-06 22:47:58 +0200

    feat: fetch the risk framework from github and api, remove the json file

--- src/utils/risk-framework.ts
+++ src/utils/risk-framework.ts
@@ -1,7 +1,34 @@
-import riskFrameworkJson from './config/risk-framework.json';
+import axios from 'axios';
+import { Network, NetworkId } from '../types';
+import { Groups, GroupingsList } from '../types/grouping';
 
-export const initRiskFrameworkScores = (network: string) => {
-    return riskFrameworkJson.groups.filter((group) =>
-        group.network.includes(network)
-    );
+const RISK_GH_URL =
+    'https://raw.githubusercontent.com/yearn/yearn-data-analytics/master/src/risk_framework/risks.json';
+const RISK_API_URL = 'https://d3971bp2359cnv.cloudfront.net/api/riskgroups/';
+
+export const initRiskFrameworkScores = async (
+    network: string
+): Promise<GroupingsList> => {
+    const index = Object.values(Network).indexOf(network as Network);
+    const key = Object.keys(Network)[index];
+    const networkId = String(NetworkId[key as keyof typeof NetworkId]);
+
+    // try to fetch directly from github first
+    const responseGithub = await axios.get(RISK_GH_URL);
+    if (responseGithub.status === 200) {
+        const riskFrameworkJson = responseGithub.data as Groups;
+        return riskFrameworkJson.groups.filter(
+            (group) => group.network === networkId
+        );
+    }
+    // try to fetch from the api
+    const responseApi = await axios.get(RISK_API_URL);
+    if (responseApi.status === 200) {
+        const riskFrameworkJson = responseApi.data as Groups;
+        return riskFrameworkJson.groups.filter(
+            (group) => group.network === networkId
+        );
+    }
+    // return empty array if not found
+    return [];
 };
